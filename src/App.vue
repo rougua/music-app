238518c8214d73668c11ad688ebacf79905bc298
rougua <guakun@aliyun.com>
2019-02-04 23:11:42 +0800

    完成 tab 组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
 
 export default {
   name: 'MusicApp',
   components: {
-    MHeader
+    MHeader, Tab
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  @import "variable";
-  #app {
-    color: $color-theme;
-  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 import fastclick from 'fastclick'
 
 import 'common/styles/index.scss'
@@ -10,5 +11,6 @@ fastclick.attach(document.body)
 // eslint-disable-next-line
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
